f7aa954af702da200828c96e1c11e76330a1a047
Pavlov Nikita <nikitapavlov2015@gmail.com>
2024-04-08 10:58:21 +0300

    fix

--- check.js
+++ check.js
@@ -49,19 +49,7 @@ const getValue = (data, spacer = '*', spacesCount = 4) => {
   return iter(data, 1);
 };
 
-// const obj = {
-//   common: {
-//     setting1: 'Value 1',
-//     setting2: {
-//       key: 'value',
-//       doge: {
-//         wow: '',
-//       },
-//     },
-//   },
-// };
 
-// console.log(getValue(obj));
 
 const getDiffResult1 = (arr) => {
   const sorted = _.sortBy(arr, ['key']);
@@ -79,7 +67,7 @@ const getDiffResult1 = (arr) => {
         break;
       }
       case 'changed': {
-        newLine = `  - ${key}: ${_.isObject(oldValue) ? getValue(oldValue) : oldValue}\n+ ${key}: ${_.isObject(value) ? getValue(value) : value}`;
+        newLine = `  - ${key}: ${_.isObject(oldValue) ? getValue(oldValue) : oldValue}\n  + ${key}: ${_.isObject(value) ? getValue(value) : value}`;
         break;
       }
       case 'unchanged': {
@@ -87,7 +75,7 @@ const getDiffResult1 = (arr) => {
         break;
       }
       case 'nested': {
-        newLine = `    ${key}: ${Array.isArray(value) ? getDiffResult1(value) : value}`;
+        newLine = `    ${key}: ${getDiffResult1(value)}`;
         break;
       }
       default: throw new Error('wrong status value');
@@ -98,10 +86,10 @@ const getDiffResult1 = (arr) => {
   return result1;
 };
 
-const firsrtFileInfo = getData('__fixtures__/file1.json');
-const secondFileInfo = getData('__fixtures__/file2.json');
-const firstFile = getParsedData(firsrtFileInfo, getFilePathExtension('__fixtures__/file1.json'));
-const secondFile = getParsedData(secondFileInfo, getFilePathExtension('__fixtures__/file2.json'));
+const firsrtFileInfo = getData('__fixtures__/file11.json');
+const secondFileInfo = getData('__fixtures__/file22.json');
+const firstFile = getParsedData(firsrtFileInfo, getFilePathExtension('__fixtures__/file11.json'));
+const secondFile = getParsedData(secondFileInfo, getFilePathExtension('__fixtures__/file22.json'));
 const result = getDiffList(firstFile, secondFile);
 // console.log(JSON.stringify(result, null, "   "));
 // console.log(JSON.stringify(firstFile, null, "   "));
@@ -114,17 +102,67 @@ console.log(getDiffResult1(result));
 // const result1 = getDiffResult1(result);
 // console.log(JSON.stringify(result1, null, "      "));
 
-    // let currentValue;
-    // if (_.isObject(value)) {
-    //   const [keyValue] = Object.keys(value);
-    //   currentValue = `{\n ${keyValue}: ${value[keyValue]} \n }`;
-    // } else {
-    //   currentValue = value;
-    // }
-    // let oldValue1;
-    // if (_.isObject(oldValue)) {
-    //   const [keyValue1] = Object.keys(oldValue);
-    //   oldValue1 = `{\n ${keyValue1}: ${oldValue[keyValue1]} \n }`;
-    // } else {
-    //   oldValue1 = oldValue;
-    // }
+// let currentValue;
+// if (_.isObject(value)) {
+//   const [keyValue] = Object.keys(value);
+//   currentValue = `{\n ${keyValue}: ${value[keyValue]} \n }`;
+// } else {
+//   currentValue = value;
+// }
+// let oldValue1;
+// if (_.isObject(oldValue)) {
+//   const [keyValue1] = Object.keys(oldValue);
+//   oldValue1 = `{\n ${keyValue1}: ${oldValue[keyValue1]} \n }`;
+// } else {
+//   oldValue1 = oldValue;
+// }
+
+// const obj = {
+//   common: {
+//     setting1: 'Value 1',
+//     setting2: {
+//       key: 'value',
+//       doge: {
+//         wow: '',
+//       },
+//     },
+//   },
+// };
+
+// console.log(getValue(obj));
+
+
+
+
+
+
+export const getDiffResult = (arr) => {
+  const sorted = _.sortBy(arr, ['key']);
+  const result = sorted.map(({
+    key, value, status, oldValue,
+  }) => {
+    let newLine;
+    switch (status) {
+      case 'added': {
+        newLine = `  + ${key}: ${value}`;
+        break;
+      }
+      case 'deleted': {
+        newLine = `  - ${key}: ${value}`;
+        break;
+      }
+      case 'changed': {
+        newLine = `  - ${key}: ${oldValue}\n  + ${key}: ${value}`;
+        break;
+      }
+      case 'unchanged': {
+        newLine = `    ${key}: ${value}`;
+        break;
+      }
+      default: throw new Error('wrong status value');
+    }
+    return newLine;
+  });
+  const result1 = ['{', ...result, '}'].join('\n');
+  return result1;
+};
--- src/functions.js
+++ src/functions.js
@@ -16,31 +16,47 @@ export const getData = (filePath) => {
 
 export const getDiffList = (data1, data2) => {
   const keys = _.union(_.keys(data1), _.keys(data2));
-
   const result = keys.reduce((acc, key) => {
-    const newAcc = {};
-    if (!_.has(data1, key)) {
-      newAcc.key = key;
-      newAcc.value = data2[key];
-      newAcc.status = 'added';
-    } else if (!_.has(data2, key)) {
-      newAcc.key = key;
-      newAcc.value = data1[key];
-      newAcc.status = 'deleted';
-    } else if (data1[key] !== data2[key]) {
-      newAcc.key = key;
-      newAcc.oldValue = data1[key];
-      newAcc.value = data2[key];
-      newAcc.status = 'changed';
-    } else {
-      newAcc.key = key;
-      newAcc.value = data1[key];
-      newAcc.status = 'unchanged';
+    if (Object.hasOwn(data1, key) && Object.hasOwn(data1, key)
+      && _.isObject(data1[key]) && _.isObject(data2[key])) {
+      const newAcc = {
+        key, status: 'nested', value: getDiffList(data1[key], data2[key]),
+      };
+      return [...acc, newAcc];
     }
+    if (!_.has(data1, key)) {
+      const newAcc = { key, status: 'added', value: data2[key] };
+      return [...acc, newAcc];
+    }
+    if (!_.has(data2, key)) {
+      const newAcc = { key, status: 'deleted', value: data1[key] };
+      return [...acc, newAcc];
+    }
+    if (data1[key] !== data2[key]) {
+      const newAcc = {
+        key, status: 'changed', oldValue: data1[key], value: data2[key],
+      };
+      return [...acc, newAcc];
+    }
+    const newAcc = { key, status: 'unchanged', value: data1[key] };
     return [...acc, newAcc];
   }, []);
-
   return result;
+};
+export const getValue = (data, spacer = '*', spacesCount = 4) => {
+  const iter = (node, depth) => {
+    if (!_.isObject(node)) {
+      return `${node}`;
+    }
+    const indentSize = depth * spacesCount;
+    const currentIndent = spacer.repeat(indentSize);
+    const bracketIndent = spacer.repeat(indentSize - spacesCount);
+    const lines = Object
+      .entries(node)
+      .map(([key, value]) => `${currentIndent}${key}: ${iter(value, depth + 1)}`);
+    return ['{', ...lines, `${bracketIndent}}`].join('\n');
+  };
+  return iter(data, 1);
 };
 
 export const getDiffResult = (arr) => {
@@ -49,21 +65,26 @@ export const getDiffResult = (arr) => {
     key, value, status, oldValue,
   }) => {
     let newLine;
+    const spacer = '****';
     switch (status) {
       case 'added': {
-        newLine = `  + ${key}: ${value}`;
+        newLine = `${spacer}**+ ${key}: ${_.isObject(value) ? getValue(value) : value}`;
         break;
       }
       case 'deleted': {
-        newLine = `  - ${key}: ${value}`;
+        newLine = `${spacer}**- ${key}: ${_.isObject(value) ? getValue(value) : value}`;
         break;
       }
       case 'changed': {
-        newLine = `  - ${key}: ${oldValue}\n  + ${key}: ${value}`;
+        newLine = `${spacer}**- ${key}: ${_.isObject(oldValue) ? getValue(oldValue) : oldValue}\n  ${spacer}+ ${key}: ${_.isObject(value) ? getValue(value) : value}`;
         break;
       }
       case 'unchanged': {
-        newLine = `    ${key}: ${value}`;
+        newLine = `${spacer}****${key}: ${_.isObject(value) ? getValue(value) : value}`;
+        break;
+      }
+      case 'nested': {
+        newLine = `${spacer}****${key}: ${getDiffResult(value)}`;
         break;
       }
       default: throw new Error('wrong status value');
