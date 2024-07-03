2acd581db0c64446e11f83c40a8d0d5de9204d2a
Pavlov Nikita <nikitapavlov2015@gmail.com>
2024-07-03 18:04:29 +0300

    fix

--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,9 @@
 import yaml from 'js-yaml';
 
 const map = {
-  json: JSON.parse,
-  yml: yaml.load,
-  yaml: yaml.load,
+  '.json': JSON.parse,
+  '.yml': yaml.load,
+  '.yaml': yaml.load,
 };
-const getParsedData = (data, fileExtension) => map[fileExtension](data);
+const getParsedData = (data, fileExtension) => map[`${fileExtension}`](data);
 export default getParsedData;
